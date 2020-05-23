dfb6a374d855095f3e8b64b1589494499e669430
Juanes165 <42305023+Juanes165@users.noreply.github.com>
2020-05-22 21:16:58 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,87 +1,55 @@
-var masa = document.getElementById("masa");
-var masaPolea = document.getElementById("masaPolea");
-var constanteElastica = document.getElementById("constanteElastica");
-var desplazamientoInicial = -5;
-var velocidadInicial = 1;
-
-var w;
-var frecuencia;
-var periodo;
-var amplitud;
-var phi;
-var tiempo = 3;
-
-var velocidad;
-var desplazamiento;
-var energiaCinetica;
-var energiaCineticaRotativa;
-var energiaPotencial;
-
-
-w = Math.sqrt(2*k/(2*masa + masaPolea));
-frecuencia = w/(2*Math.PI);
-periodo = 1/frecuencia;
-phi = Math.atan(w*desplazamientoInicial/velocidadInicial);
-
-
-if(phi != 0)
-{
-    amplitud = desplazamientoInicial/Math.sin(phi);
-}
-else
-{
-    amplitud = velocidadInicial/(w*Math.cos(phi));
+function roundByDecimals(number, decimals) {
+    const tenPow = Math.pow(10, decimals)
+    return Math.round(number * tenPow) / tenPow;
 }
 
-desplazamiento = amplitud*Math.sin(w*tiempo + phi);
-velocidad = w*amplitud*Math.cos(w*tiempo + phi);
+
+function naturalFrequency(springK, mass1, mass2) {
+    return Math.sqrt(2 * springK / (2 * mass1 + mass2));
+}
 
 
-energiaCinetica = masa*(Math.pow(velocidad,2))/2 + (masaPolea*Math.pow(velocidad,2))/4;
-energiaPotencial = k*(Math.pow(desplazamiento,2))/2;
+function frequency(w) {
+    return w / (2 * Math.PI);
+}
 
 
-masa = Math.round(masa*1000)/1000;
-k = Math.round(k*1000)/1000;
-desplazamientoInicial = Math.round(desplazamientoInicial*1000)/1000;
-velocidadInicial = Math.round(velocidadInicial*1000)/1000;
-w = Math.round(w*1000)/1000;
-frecuencia = Math.round(frecuencia*1000)/1000;
-periodo = Math.round(periodo*1000)/1000;
-amplitud = Math.round(amplitud*1000)/1000;
-phi = Math.round(phi*1000)/1000;
-tiempo = Math.round(tiempo*1000)/1000;
-velocidad = Math.round(velocidad*1000)/1000;
-desplazamiento = Math.round(desplazamiento*1000)/1000;
-energiaCinetica = Math.round(energiaCinetica*1000)/1000;
-energiaPotencial = Math.round(energiaPotencial*1000)/1000;
+function period(freq) {
+    return 1 / freq;
+}
 
 
+function phiCalc(w, initDisp, initVel) {
+    return Math.atan(w * initDisp / initVel);
+}
 
-document.write("Masa: ", masa, " kg" ,"<br>")
-document.write("Masa Polea: ", masaPolea, " kg", "<br>")
-document.write("Constante elastica: ", k, " N/m", "<br>")
-document.write("Frecuencia(rad): ", w, " Rad/s", "<br>")
-document.write("Frecuencia(Hz): ", frecuencia, " Hz", "<br>")
-document.write("Periodo: ", periodo, " seg", "<br>")
-document.write("Amplitud: ", amplitud, "<br>")
-document.write(String.fromCharCode(934) + ":", phi, " Rad", "<br>")
-document.write("Tiempo: ", tiempo, " seg", "<br>")
-document.write("Desplazamiento: ", desplazamiento, " mts" , "<br>")
-document.write("Velocidad: ", velocidad, " m/s", "<br>")
-document.write("Energia cinetica: ", energiaCinetica, " J", "<br>")
-document.write("Energia potencial: ", energiaPotencial, " J", "<br>")
 
-/*
-console.log("W:", w);
-console.log("frecuencia:", frecuencia);
-console.log("periodo:", periodo);
-console.log("amplitud:", amplitud);
-console.log(String.fromCharCode(966) + ":", phi);
-console.log("tiempo:", tiempo);
-console.log("velocidad:", velocidad);
-console.log("desplazamiento:", desplazamiento);
-console.log("energia cinetica:", energiaCinetica);
-console.log("energia potencial:", energiaPotencial);
+function amplitude(w, initDisp, initVel) {
+    let phi = phiCalc(w, initDisp, initVel)
+    if (phi != 0) {
+        return initDisp / Math.sin(phi);
+    }
+    else {
+        return initVel / (w * Math.cos(phi));
+    }
+}
 
-//console.log(String.fromCharCode(934));*/
+
+function displacement(amp, w, time, phi) {
+    return amp * Math.sin(w * time + phi);
+}
+
+
+function velocity(amp, w, time, phi) {
+    return w * amp * Math.cos(w * time + phi);
+}
+
+
+function kineticEnergy(mass1, mass2, velocity) {
+    return mass1 * (Math.pow(velocity, 2)) / 2 + (mass2 * Math.pow(velocity, 2)) / 4;
+}
+
+
+function potentialEnergy(springK, displacement) {
+    return springK * (Math.pow(displacement, 2)) / 2;
+}
