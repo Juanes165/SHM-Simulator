884e8dcdabae20725d0411f5ab51fd35f4fca972
Juanes165 <42305023+Juanes165@users.noreply.github.com>
2020-05-23 19:31:23 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -4,28 +4,40 @@ function roundByDecimals(number, decimals) {
 }
 
 
-function naturalFrequency(springK, mass1, mass2) {
+function naturalFrequencyCalc(springK, mass1, mass2) {
+    
     return Math.sqrt(2 * springK / (2 * mass1 + mass2));
 }
 
 
-function frequency(w) {
+function frequencyCalc(springK, mass1, mass2) {
+
+    let w = naturalFrequencyCalc(springK, mass1, mass2)
+
     return w / (2 * Math.PI);
 }
 
 
-function period(freq) {
+function periodCalc(springK, mass1, mass2) {
+
+    let freq = frequencyCalc(springK, mass1, mass2)
+
     return 1 / freq;
 }
 
 
-function phiCalc(w, initDisp, initVel) {
+function phiCalc(springK, mass1, mass2, initDisp, initVel) {
+
+    let w = naturalFrequencyCalc(springK, mass1, mass2);
     return Math.atan(w * initDisp / initVel);
 }
 
 
-function amplitude(w, initDisp, initVel) {
-    let phi = phiCalc(w, initDisp, initVel)
+function amplitudeCalc(springK, mass1, mass2, initDisp, initVel) {
+    
+    let w = naturalFrequencyCalc(springK, mass1, mass2);
+    let phi = phiCalc(springK, mass1, mass2, initDisp, initVel);
+
     if (phi != 0) {
         return initDisp / Math.sin(phi);
     }
@@ -35,18 +47,28 @@ function amplitude(w, initDisp, initVel) {
 }
 
 
-function displacement(amp, w, time, phi) {
+function displacementCalc(springK, mass1, mass2, initDisp, initVel, time) {
+
+    let w = naturalFrequencyCalc(springK, mass1, mass2)
+    let amp = amplitudeCalc(springK, mass1, mass2, initDisp, initVel);
+    let phi = phiCalc(springK, mass1, mass2, initDisp, initVel);
+
     return amp * Math.sin(w * time + phi);
 }
 
 
-function velocity(amp, w, time, phi) {
+function velocityCalc(springK, mass1, mass2, initDisp, initVel, time) {
+
+    let w = naturalFrequencyCalc(springK, mass1, mass2)
+    let amp = amplitudeCalc(springK, mass1, mass2, initDisp, initVel);
+    let phi = phiCalc(springK, mass1, mass2, initDisp, initVel);
+
     return w * amp * Math.cos(w * time + phi);
 }
 
 
 function kineticEnergy(mass1, mass2, velocity) {
-    return mass1 * (Math.pow(velocity, 2)) / 2 + (mass2 * Math.pow(velocity, 2)) / 4;
+    return (mass1 + mass2 / 2) * (Math.pow(velocity, 2)) / 2
 }
 
 
